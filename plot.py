e2083d2ed2c82d629d7d6904e8851354fa587b86
Sergiu Moga <sergiu.moga@protonmail.com>
2022-03-21 22:01:17 +0200

    Improve plot.py
    The resulted file of `strace -tt /path/to/binary 2>results_file`
    can now be automatically parsed and separate
    syscall_names/syscall_timestamps files will be generated.
    Give color magenta to dots that take place beore the benchmark.

--- plot.py
+++ plot.py
@@ -2,12 +2,30 @@
 
 import matplotlib.pyplot as plt
 from datetime import datetime
+import sys
+import os
 
-with open("strace-syscalls") as file:
-    syscalls = file.read().splitlines()
+# sys.argv[1] - strace output file from `strace -tt path/to/binary 2>strace_outfile`
+# sys.argv[2] - syscall names file
+# sys.argv[3] - syscall timestamps file
+# sys.argv[4] - command delay
 
-with open("strace-timestamps") as file:
-    timestamps = file.read().splitlines()
+setup_script = sys.argv[1]
+strace_outfile = sys.argv[2]
+syscall_names = sys.argv[3]
+syscall_timestamps = sys.argv[4]
+cmd_delay = sys.argv[5]
+
+os.system(setup_script + " " + \
+          strace_outfile + " " + \
+          syscall_names + " " + \
+          syscall_timestamps)
+
+with open(syscall_names) as f:
+    syscalls = f.read().splitlines()
+
+with open(syscall_timestamps) as f:
+    timestamps = f.read().splitlines()
 
 fmt = "%H:%M:%S.%f"
 last_timestamp = datetime.strptime(timestamps[-1], fmt)
@@ -18,5 +36,10 @@ for t in timestamps:
 
 delta_timestamps = list(reversed(delta_timestamps))
 
-plt.plot(delta_timestamps, syscalls, 'o', color='black')
+for dot in range(len(delta_timestamps)):
+    if delta_timestamps[dot] <= float(cmd_delay):
+        plt.scatter(delta_timestamps[dot], syscalls[dot], c = "magenta")
+    else:
+        plt.scatter(delta_timestamps[dot], syscalls[dot], c = "blue")
+
 plt.show()
